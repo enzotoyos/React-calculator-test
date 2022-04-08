ac32e54f7f9c3b2160d4a57fc49cbf7b8e9c3abb
enzotoyos <enzo.toyos@gmail.com>
2022-04-08 11:01:57 +0200

    modif readme

--- src/tests/hooks/useRandomUser.test.tsx
+++ src/tests/hooks/useRandomUser.test.tsx
@@ -1,4 +1,3 @@
-// __tests__/fetch.test.js
 import React from "react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -6,34 +5,34 @@ import { render, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../../App";
 
-const server = setupServer(
-    rest.get(
-        "https://randomuser.me/api/",
-        (req, res, ctx) => {
-            return res(
-                ctx.json({
-                    result: [{
-                        picture: {
-                            "thumbnail": "https://randomuser.me/api/portraits/thumb/women/92.jpg"
-                        }
-                        // name: {
-                        //     title: "Miss",
-                        //     first: "Helen",
-                        //     last: "Souza"
-                        // }
-                    }]
-                })
-            );
-        }
-    )
+const server2 = setupServer(
+  rest.get("https://randomuser.me/api/", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        results: [
+          {
+            picture: {
+              thumbnail:
+                "https://randomuser.me/api/portraits/thumb/women/92.jpg",
+            },
+            name: {
+              title: "Miss",
+              first: "Helen",
+              last: "Souza",
+            },
+          },
+        ],
+      })
+    );
+  })
 );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+beforeAll(() => server2.listen());
+afterEach(() => server2.resetHandlers());
+afterAll(() => server2.close());
 
 test("load random user mock", async () => {
-    const { container } = render(<App />);
-    await waitFor(() => screen.getByText("Utilisateur"));
-    expect(container.getElementsByTagName('img')).toBe(1);
+  const { container } = render(<App />);
+  await waitFor(() => screen.getByText("Utilisateur"));
+  expect(container.getElementsByTagName("img").length).toBe(1);
 });
